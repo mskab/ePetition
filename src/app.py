72af56544d0abd24a221c8e99cba3f077fc48aa4
mskab <mskab2000@gmail.com>
2022-07-22 21:41:14 +0300

    remove unnecessary logic

--- src/app.py
+++ src/app.py
@@ -2,12 +2,7 @@ from flask import Flask
 from .config import app_config
 
 def create_app(env_name):
-  """
-  Create app
-  """
-  
   app = Flask(__name__)
-
   app.config.from_object(app_config[env_name])
 
   @app.route('/', methods=['GET'])
