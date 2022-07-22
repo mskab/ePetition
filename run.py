664420508685a5e4afc0fe13f2f807c8d1731032
mskab <mskab2000@gmail.com>
2022-07-22 21:38:54 +0300

    Add .env file and update pipfile

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 from src.app import create_app
 
 if __name__ == '__main__':
-  env_name = os.getenv('FLASK_ENV')
+  env_name = os.environ['FLASK_ENV']
   app = create_app(env_name)
   app.run()
--- src/config.py
+++ src/config.py
@@ -1,30 +1,33 @@
 import os
+from dotenv import load_dotenv
 
-class Development(object):
+load_dotenv()
+
+class Development():
     """
     Development environment configuration
     """
     DEBUG = True
     TESTING = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
-class Production(object):
+class Production():
     """
     Production environment configurations
     """
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
 
-class Testing(object):
+class Testing():
     """
-    Configurations for Testing, with a separate test database
+    Testing environment configurations
     """
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 app_config = {
     'development': Development,
