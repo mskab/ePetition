c032996dbebeabba546c198f9a634bfd9ae4f9b8
mskab <mskab2000@gmail.com>
2022-08-03 15:28:49 +0300

    Add decision maker endpoints

--- api/routes/base.py
+++ api/routes/base.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
-from routes import user, petition
+from routes import user, petition, decision_maker
 
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(
     petition.router, prefix="/petitions", tags=["petitions"])
+api_router.include_router(decision_maker.router,
+                          prefix="/decision_makers", tags=["decision_makers"])
