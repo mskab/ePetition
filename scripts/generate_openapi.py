4d990e8fbfaf93a6d7b6e48338be06d71b579a24
mskab <mskab2000@gmail.com>
2022-10-04 00:52:05 +0300

    Add git action for creating api docs

---
+++ scripts/generate_openapi.py
@@ -0,0 +1,33 @@
+import importlib
+import os
+import sys
+
+import yaml
+from fastapi.openapi.utils import get_openapi
+
+
+workspace = os.environ.get("GITHUB_WORKSPACE")
+sys.path.append(workspace)
+
+os.system("pip install pipenv --upgrade && pipenv lock")
+
+try:
+    mod = importlib.import_module("api.main")
+except Exception as e:
+    raise ModuleNotFoundError(
+        f"Error importing api/main.py file."
+        f"Error: {e}"
+    )
+
+app = getattr(mod, "ePetition")
+
+specs = get_openapi(
+    title=app.title if app.title else None,
+    version=app.version if app.version else None,
+    openapi_version=app.openapi_version if app.openapi_version else None,
+    description=app.description if app.description else None,
+    routes=app.routes if app.routes else None,
+)
+
+with open("openapi.yml", "w") as f:
+    yaml.dump(specs, f)
