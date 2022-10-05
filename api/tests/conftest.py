9e46e31bc589ab5fb15144dd0634d81d64446d02
mskab <mskab2000@gmail.com>
2022-10-05 13:50:16 +0300

    Update conftest

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,5 +1,4 @@
 import os
-print("--------------------------------------------------",os.getcwd())
 from typing import Any
 from typing import Generator
 
@@ -13,7 +12,8 @@ from utils.users import authentication_token_from_email, register_admin_to_db
 
 import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.chdir(os.path.dirname(os.getcwd()))
 
 from api.db.base import Base
 from api.db.session import get_db
